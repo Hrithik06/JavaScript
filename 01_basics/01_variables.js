4273139f27dc34ace5d6d6e700c0e497500042b2
Hrithik <47109824+Hrithik06@users.noreply.github.com>
2024-02-13 10:58:13 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,15 @@
 const accId = 12
 let accEmail = "hrithik@email.com"
 var accPasswrod = "123456"
-accCity = "Pune"
+accCity = "Pune"// also possible
+let accState;
+// accId = 45 //not allowed
+accEmail = "hrithik@hrithik.com"
+accPasswrod = "21123"
+accCity = "Delhi"
 
-console.table([accId, accEmail, accPasswrod, accCity]);
+/*
+Prefer not to use var,
+cuz of issue of block-scope
+ */
+console.table([accId, accEmail, accPasswrod, accCity, accState]);
--- 01_basics/Notes.md
+++ 01_basics/Notes.md
@@ -1,4 +1,5 @@
 3 ways to declare variables in JS.
-- const
-- let
-- var
+- const: to declare constants, as the name sames they can't bed mutated once declared.
+- let: to declare variables, which can be edited, within thw scope. Solves the issue of scope.
+- var: same as let, used before let was intrdouced no concept of scope.
+- Decalring variable without initial will be assigned undefined.
